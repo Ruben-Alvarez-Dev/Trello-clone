31fb6750d6371d436e02c0682e26bb02ddb5f271
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2024-03-25 14:10:23 +0100

    editting task title going right

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,41 +1,68 @@
 import './Task.css';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../contexts/DataContext";
 import trash from "../assets/trash.svg";
 
 export const Task = ({ task, taskIndex }) => {
-  
-  const { lists, setLists, tasks, setTasks } = useContext(DataContext)
-  
+  const { lists, setLists, tasks, setTasks } = useContext(DataContext);
+  const [editable, setEditable] = useState(false);
+  const [taskValue, setTaskValue] = useState(task.value);
+
   const handleRemoveTask = (e) => {
     const taskId = e.target.parentNode.parentNode.id;
-    
     const updatedLists = lists.map((list) => {
       if (list.value.includes(taskId)) {
-        return {
-          ...list,
-          value: list.value.filter((id) => id !== taskId),
-        };
+        return { ...list, value: list.value.filter((id) => id !== taskId) };
       }
       return list;
     });
-  
     const updatedTasks = tasks.filter((task) => task.id !== taskId);
-  
     setLists(updatedLists);
     setTasks(updatedTasks);
   };
 
-  return (
+  const handleEditTask = () => {
+    setEditable(true);
+  };
 
-        <div className='task' id={task.id}>
-          <div className="task-title">
-            {task.value}
-          </div>
-          <div className="task-remove" onClick={handleRemoveTask}>
-            <img src={trash}/>
-          </div>
+  const handleTaskChange = (e) => {
+    setTaskValue(e.target.value);
+  };
+
+  const handleTaskBlur = () => {
+    if (taskValue.trim() !== '') {
+      const updatedTasks = tasks.map((item) => {
+        if (item.id === task.id) {
+          return { ...item, value: taskValue };
+        }
+        return item;
+      });
+      setTasks(updatedTasks);
+    } else {
+      setTaskValue(task.value);
+    }
+    setEditable(false);
+  };
+
+  return (
+    <div className='task' id={task.id}>
+      {editable ? (
+        <input
+          className="input"
+          type="text"
+          value={taskValue}
+          onChange={handleTaskChange}
+          onBlur={handleTaskBlur}
+          autoFocus
+        />
+      ) : (
+        <div className="task-title" onDoubleClick={handleEditTask}>
+          {task.value}
         </div>
-      
-  )
-}
+      )}
+      <div className="task-remove" onClick={handleRemoveTask}>
+        <img src={trash} alt="Remove" />
+      </div>
+    </div>
+  );
+};
