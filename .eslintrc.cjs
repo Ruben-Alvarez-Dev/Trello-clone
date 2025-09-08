c3762187718a37bd49edfb0581f870c37657c19c
Ruben-Alvarez-Dev <ruben.alvarez.dev@gmail.com>
2025-09-08 17:27:57 +0200

    chore: add Node engine (>=18) and .nvmrc; relax ESLint for docs/React imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,15 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', 'docs', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-unused-vars': ['error', { varsIgnorePattern: '^React$' }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
